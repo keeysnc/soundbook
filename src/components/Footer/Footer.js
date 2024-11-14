88c7330e5f52b1cf0fe075a6a1f7960c830a7a07
Nathan Keeys <nathankeeys@i-see-you.myfiosgateway.com>
2024-11-14 02:17:02 -0500

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,31 +10,32 @@ const Footer = () => {
 			<div>
 				<div className={footerStyles.footer_bg}>
 					<div className={footerStyles.footer_container}>
-						<a href="/" className="pb-4">
-							<svg width="75" height="75" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-								{/* Inner Circle */}
-								<circle cx="100" cy="100" r="70" stroke="white" strokeWidth="6" fill="none" />
+						<div className="px-4">
+							<a href="/" className="pb-4">
+								<svg width="75" height="75" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+									{/* Inner Circle */}
+									<circle cx="100" cy="100" r="70" stroke="white" strokeWidth="6" fill="none" />
 
-								{/* Text Path Definition */}
-								<defs>
-									<path id="textPath" d="M 100, 100 m -80, 0 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0" />
-								</defs>
+									{/* Text Path Definition */}
+									<defs>
+										<path id="textPath" d="M 100, 100 m -80, 0 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0" />
+									</defs>
 
-								{/* Rotating Text */}
-								<text fontSize="30" fontFamily="Bebas" fill="white">
-									<textPath
-										href="#textPath"
-										startOffset="0%"
-										style={{
-											animation: "rotateText 8s linear infinite",
-										}}
-									>
-										SOUNDBOOK
-									</textPath>
-								</text>
+									{/* Rotating Text */}
+									<text fontSize="30" fontFamily="Bebas" fill="white">
+										<textPath
+											href="#textPath"
+											startOffset="0%"
+											style={{
+												animation: "rotateText 8s linear infinite",
+											}}
+										>
+											SOUNDBOOK
+										</textPath>
+									</text>
 
-								<style>
-									{`
+									<style>
+										{`
                   text {
                     transform-origin: 100px 100px; /* Center the rotation */
                     animation: rotateText 8s linear infinite;
@@ -45,29 +46,30 @@ const Footer = () => {
                     100% { transform: rotate(360deg); }
                   }
                 `}
-								</style>
-							</svg>
-						</a>
-						{/* <ul>
+									</style>
+								</svg>
+							</a>
+							{/* <ul>
 							<li className="pb-2">Payment Methods</li>
 							<li className="pb-2">Shipping and Delivery</li>
 							<li className="pb-2">Return & Refund</li>
 						</ul> */}
-						<p className="text-[16px]">Digital Soundbook Curation [2024]</p>
-						<b>
-							<p className="italic text-[16px]">
-								Curated with lofi beat produced by{" "}
-								<a
-									target="_blank"
-									className="underline cursor-pointer"
-									href="https://open.spotify.com/artist/5e8tG9jOxUPyK6IOQ3ZDSg?si=BOAgRKf9Tb-Y2mtLrkERzQ"
-								>
-									80% Cacao
-								</a>
-							</p>
-						</b>
-						<br />
-						<small>Copyright 2024 - Digital Anthro, LLC </small>
+							<p className="text-[16px]">Digital Soundbook Curation [2024]</p>
+							<b>
+								<p className="italic text-[16px]">
+									Curated with lofi beat produced by{" "}
+									<a
+										target="_blank"
+										className="underline cursor-pointer"
+										href="https://open.spotify.com/artist/5e8tG9jOxUPyK6IOQ3ZDSg?si=BOAgRKf9Tb-Y2mtLrkERzQ"
+									>
+										80% Cacao
+									</a>
+								</p>
+							</b>
+							<br />
+							<small>Copyright 2024 - Digital Anthro, LLC </small>
+						</div>
 					</div>
 				</div>
 			</div>
